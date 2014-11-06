a1f78e08415a9079df8725072208b93046850ea6
Corey Haines <coreyhaines@gmail.com>
2014-11-06 09:25:17 -0600

    Caesar cypher. Not 100% sure that the offsets are working if the offset makes it wrap more than once, but I'm feeling too lazy to figure it out the exact numbers. :)

--- my_list.exs
+++ my_list.exs
@@ -7,4 +7,14 @@ defmodule MyList do
   defp _max([], current_max), do: current_max
   defp _max([h|t], current_max) when h <= current_max, do: _max(t, current_max)
   defp _max([h|t], _), do: _max(t, h)
+
+
+  def caesar([], _), do: []
+  def caesar([char|t], n), do: [handle_caesar_letter(char, n) | caesar(t, n)]
+
+  defp _handle_caesar_letter(char, offset) when (char + offset) <= ?z, do: char + offset
+  defp _handle_caesar_letter(char, offset) do
+    amount_over_z = rem((char + offset), ?z + 1)
+    handle_caesar_letter(?a, amount_over_z)
+  end
 end
